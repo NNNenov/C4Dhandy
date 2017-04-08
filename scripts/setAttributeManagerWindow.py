d5f87426fee9a5cfe99810bb38042a768fd49c0c
NNNenov <n@nnenov.eu>
2017-04-08 16:23:14 +0100

    added new script
    Modify this script based on what you want your attribute manager to set.
    It doesn't always work since I think it depends on what is available in your scene, I haven't fully looked into it yet.
    This is a quick bit o code I got thanks to gr4ph0s via http://plugincafe.com/forum/forum_posts.asp?TID=13525
    relevant SDK link: 
    https://developers.maxon.net/docs/Cinema4DPythonSDK/html/modules/c4d.gui/index.html?highlight=manager#c4d.gui.ActiveObjectManager_SetObject

---
+++ scripts/setAttributeManagerWindow.py
@@ -0,0 +1,25 @@
+import c4d
+
+op = c4d.documents.GetActiveDocument() 
+
+def main():
+    c4d.gui.ActiveObjectManager_SetObject(c4d.ACTIVEOBJECTMODE_SHADER, op, c4d.ACTIVEOBJECTMANAGER_SETOBJECTS_OPEN)
+    
+    #ACTIVEOBJECTMODE_OBJECT	Object mode.
+    #ACTIVEOBJECTMODE_TAG	Tag mode.
+    #ACTIVEOBJECTMODE_MATERIAL	Material mode.
+    #ACTIVEOBJECTMODE_SHADER	Shader mode.
+    #ACTIVEOBJECTMODE_NODE	Node mode.
+    #ACTIVEOBJECTMODE_TIMELINE	Time-line mode.
+    #ACTIVEOBJECTMODE_FCURVE	F-curve mode.
+    #ACTIVEOBJECTMODE_BITMAPINFO	BodyPaint bitmap info mode.
+    #ACTIVEOBJECTMODE_TOOL	Tool mode.
+    #ACTIVEOBJECTMODE_VIEW	View mode.
+    #ACTIVEOBJECTMODE_INFOTAB	Info tab.
+    #ACTIVEOBJECTMODE_CAMERA	Editor camera mode.
+    #ACTIVEOBJECTMODE_RENDERDATA	Render data mode.
+    #ACTIVEOBJECTMODE_DOCUMENT	Document settings mode.
+    #ACTIVEOBJECTMODE_MODELING	Modeling mode.
+
+if __name__=='__main__':
+    main()
